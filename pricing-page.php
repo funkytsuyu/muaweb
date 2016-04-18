416cdac86b2284419d828fd6a98645ced1d236ea
oilandsugar <tommy_pm@hotmail.com>
2016-04-17 21:49:43 -0400

    add pricing page

---
+++ pricing-page.php
@@ -0,0 +1,60 @@
+<?php
+// template name: pricing page
+
+// data
+
+?>
+
+<?php get_header(); ?>
+<section id="content" role="main" class="wrap">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+</header>
+<section class="entry-content">
+  <div class="half">
+    <h2>Fashion, beauty and editorial packages</h2>
+    <div class="package">
+    <div class="title">
+    <div class="price">50$</div>
+    “3 and less” Package
+
+    </div>
+    Perfect for you and two of your friends or for a couple.
+    * For Men : 40$/each
+
+    </div>
+    <div class="package">
+    <div class="title">
+    <div class="price">40$</div>
+    “4 and more” Package
+
+    </div>
+    Get a discount by bringing more people to the session, ideal for bigger groups.
+    * For Men : 30$/each
+
+    </div>
+    <div class="package">
+    <div class="title">
+    <div class="price">45$/hour</div>
+    Rates
+
+    </div>
+    If none of the above packages is what you are looking for, a pay-per-hour option is also available to accommodate your needs.
+    Half a day (5 hours): 200$
+    Full day (8 hours): 320$
+
+    * Travel expenses are not included.
+
+    </div>
+  </div>
+  <div class="half right">
+    <h2>Wedding packages</h2>
+  </div>
+
+</section>
+</article>
+<?php if ( ! post_password_required() ) comments_template('', true); ?>
+<?php endwhile; endif; ?>
+</section>
+<?php get_footer(); ?>
