e1ee0aa48c9e5639512c412fd200d192cd459562
oilandsugar <tommy_pm@hotmail.com>
2016-04-17 22:00:14 -0400

    add pricing page

--- pricing-page.php
+++ pricing-page.php
@@ -13,39 +13,25 @@
 <section class="entry-content">
   <div class="half">
     <h2>Fashion, beauty and editorial packages</h2>
+    <?php
+    $genpacksarrays = simple_fields_get_post_group_values(get_the_id(), "General packages", true, 1);
+    $genpacktitles = $genpacksarrays["Name"];
+    $genpackprices = $genpacksarrays["Price"];
+    $genpackdescs = $genpacksarrays["Desc"];
+    $genpacknum  = count($genpacktitles);
+    for($i = 0; $i < $genpacknum; $i++) {
+      $title = $genpacktitles[$i];
+      $price = $genpackprices[$i];
+      $desc = $genpackdescs[$i];
+    ?>
     <div class="package">
-    <div class="title">
-    <div class="price">50$</div>
-    “3 and less” Package
-
+      <div class="title">
+        <div class="price"><?php echo $price; ?></div>
+    <?php echo $title; ?>
     </div>
-    Perfect for you and two of your friends or for a couple.
-    * For Men : 40$/each
-
+    <?php echo $desc; ?>
     </div>
-    <div class="package">
-    <div class="title">
-    <div class="price">40$</div>
-    “4 and more” Package
-
-    </div>
-    Get a discount by bringing more people to the session, ideal for bigger groups.
-    * For Men : 30$/each
-
-    </div>
-    <div class="package">
-    <div class="title">
-    <div class="price">45$/hour</div>
-    Rates
-
-    </div>
-    If none of the above packages is what you are looking for, a pay-per-hour option is also available to accommodate your needs.
-    Half a day (5 hours): 200$
-    Full day (8 hours): 320$
-
-    * Travel expenses are not included.
-
-    </div>
+    <?php } ?>
   </div>
   <div class="half right">
     <h2>Wedding packages</h2>
