56c626894859b228969e75f2dc75ad7c55b5d797
oilandsugar <tommy_pm@hotmail.com>
2016-04-17 21:52:14 -0400

    add pricing page

--- pricing-page.php
+++ pricing-page.php
@@ -7,7 +7,6 @@
 
 <?php get_header(); ?>
 <section id="content" role="main" class="wrap">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header class="header">
 </header>
@@ -54,7 +53,5 @@
 
 </section>
 </article>
-<?php if ( ! post_password_required() ) comments_template('', true); ?>
-<?php endwhile; endif; ?>
 </section>
 <?php get_footer(); ?>
