11ed958279b04abdb9ab45944d13e2d7e09014cf
oilandsugar <tommy_pm@hotmail.com>
2016-05-29 22:11:14 -0400

    test instagram

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <section id="content" role="main" class="wrap">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part('entry'); ?>
-<?php echo wdi_feed(array('id'=>'1')); ?>
 <?php endwhile; endif; ?>
 <?php get_template_part('nav', 'below'); ?>
 </section>
