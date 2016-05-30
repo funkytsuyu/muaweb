03d2ca7347e348d6cff433ac798aadbb9bda3309
oilandsugar <tommy_pm@hotmail.com>
2016-05-29 21:01:04 -0400

    remove cat header

--- category.php
+++ category.php
@@ -1,57 +1,5 @@
 <?php get_header(); ?>
 <section id="content" role="main" class="wrap">
-	<?php if(is_category('beauty')) { ?>
-		<header class="header ">
-			<?php
-			$recent = new WP_Query("page_id=34");
-			while ($recent->have_posts()) : $recent->the_post();
-				the_content();
-			endwhile;
-			wp_reset_postdata();
-			?>
-		</header>
-	<?php } elseif(is_category('creative')) { ?>
-		<header class="header ">
-			<?php
-			$recent = new WP_Query("page_id=37");
-			while ($recent->have_posts()) : $recent->the_post();
-				the_content();
-			endwhile;
-			wp_reset_postdata();
-			?>
-		</header>
-	<?php } elseif(is_category('fashion')) { ?>
-		<header class="header">
-			<?php
-			$recent = new WP_Query("page_id=39");
-			while ($recent->have_posts()) : $recent->the_post();
-				the_content();
-			endwhile;
-			wp_reset_postdata();
-			?>
-		</header>
-	<?php } elseif(is_category('wedding')) { ?>
-		<header class="header">
-			<?php
-			$recent = new WP_Query("page_id=41");
-			while ($recent->have_posts()) : $recent->the_post();
-				the_content();
-			endwhile;
-			wp_reset_postdata();
-			?>
-		</header>
-	<?php } elseif(is_category('seancesthematiques')) { ?>
-		<header class="header">
-			<?php
-			$recent = new WP_Query("page_id=471");
-			while ($recent->have_posts()) : $recent->the_post();
-				the_content();
-			endwhile;
-			wp_reset_postdata();
-			?>
-		</header>
-	<?php } ?>
-
 	<div id="masonry-cat" class="projects">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<div class="project square">
