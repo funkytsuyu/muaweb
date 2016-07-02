515a072ca773ae624a80841c3d6417c4409e6d8b
oilandsugar <tommy_pm@hotmail.com>
2016-07-01 21:50:02 -0400

    fix slider

--- js/script.js
+++ js/script.js
@@ -67,6 +67,11 @@ jQuery(function($) {
 		]
 	});
 
+	var $projectgallery = $('.project-gallery');
+	$projectgallery.imagesLoaded( function() {
+		$('.project-gallery').animate({opacity: 1}, 700);
+	}
+
 	// slick for about page
 
 	$('.publications').slick({
