3b8f17d2ba292ee581d7b62a5735e9aa86c64ff0
oilandsugar <tommy_pm@hotmail.com>
2016-07-25 21:39:21 -0400

    change about page

--- about-page.php
+++ about-page.php
@@ -20,6 +20,7 @@ $photourl = wp_get_attachment_url($photo);
     <div class="flex-1"><img src="<?php echo $photourl; ?>"></div>
     <div class="flex-2"><div class='padding-left'><?php echo $bio; ?></div></div>
     <div class="collabs flex-2">
+      <div class='padding-left'>
       <?php if($collab_title) { echo '<h2>' . $collab_title . '</h2>'; } ?>
       <?php
       $collabarrays = simple_fields_get_post_group_values(get_the_id(), "Collaborations", true, 1);
@@ -42,6 +43,7 @@ $photourl = wp_get_attachment_url($photo);
           } ?>
       </div>
       <?php } ?>
+      </div>
     </div>
   </div>
 
