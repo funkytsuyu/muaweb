0a4f975a6c7a0eb34b7ed1e3f84597a67d5a38cd
oilandsugar <tommy_pm@hotmail.com>
2016-08-12 08:23:32 -0400

    change isotope for masonry

--- js/script.js
+++ js/script.js
@@ -124,24 +124,24 @@ jQuery(function($) {
 		$('.mobile-menu-button-icon-open').animate({opacity: 0}, 500);
 	});
 
-	$.fn.masonryImagesReveal = function( $items ) {
-	  var msnry = this.data('masonry');
-	  var itemSelector = msnry.options.itemSelector;
-	  // hide by default
-	  $items.hide();
-	  // append to container
-	  this.append( $items );
-	  $items.imagesLoaded().progress( function( imgLoad, image ) {
-	    // get item
-	    // image is imagesLoaded class, not <img>, <img> is image.img
-	    var $item = $( image.img ).parents( itemSelector );
-	    // un-hide item
-	    $item.show();
-	    // masonry does its thing
-	    msnry.appended( $item );
-	  });
-
-	  return this;
-	};
-
 });
+
+$.fn.masonryImagesReveal = function( $items ) {
+	var msnry = this.data('masonry');
+	var itemSelector = msnry.options.itemSelector;
+	// hide by default
+	$items.hide();
+	// append to container
+	this.append( $items );
+	$items.imagesLoaded().progress( function( imgLoad, image ) {
+		// get item
+		// image is imagesLoaded class, not <img>, <img> is image.img
+		var $item = $( image.img ).parents( itemSelector );
+		// un-hide item
+		$item.show();
+		// masonry does its thing
+		msnry.appended( $item );
+	});
+
+	return this;
+};
