60c00c05a6ef90ccb63e2d3c833249405423f97b
oilandsugar <tommy_pm@hotmail.com>
2016-08-11 22:38:31 -0400

    fix home slider

--- home-page.php
+++ home-page.php
@@ -8,15 +8,17 @@
     $bannerarrays = simple_fields_get_post_group_values(get_the_id(), "Banners", true, 1);
     $bannerimgs = $bannerarrays["Image"];
     $bannerlinks = $bannerarrays["Link"];
+    $bannerbackcolors = $bannerarrays["back color"];
     $bannernum  = count($bannerimgs);
     for($i = 0; $i < $bannernum; $i++) {
       $img = $bannerimgs[$i];
       $link = $bannerlinks[$i];
+      $back_color = $bannerbackcolors[$i];
       $imgurl = wp_get_attachment_url($img);
     ?>
     <div class="banner">
       <?php if($link) { echo '<a href="' . $link . '">'; } ?>
-        <div class="img">
+        <div class="img" style="background-color:<?php echo $back_color;">
           <?php echo '<img src="' . $imgurl . '">'; ?>
         </div>
       <?php if($link) { echo '</a>'; } ?>
