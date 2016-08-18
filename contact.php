b6695d3a974b00cf5a30d069e8e12319d6aaa6f1
oilandsugar <tommy_pm@hotmail.com>
2016-08-18 18:57:55 -0400

    update contact page

--- contact.php
+++ contact.php
@@ -1,27 +1,38 @@
 <?php
 /* template name: contact */
+
+// Data
+$map = simple_fields_value('map');
+$map_url = wp_get_attachment_url($map);
+$contact_text = simple_fields_value('contact_text');
 ?>
 
 <?php get_header(); ?>
-<section id="content" role="main" class="wrap">
-	<div class="half">
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="header">
-				</header>
-				<section class="entry-content">
-					<?php the_content(); ?>
-					<div class="entry-links"><?php wp_link_pages(); ?></div>
-				</section>
-			</article>
-			<?php if ( ! post_password_required() ) comments_template('', true); ?>
-		<?php endwhile; endif; ?>
-	</div>
-	<div class="half">
-		<h3>
-			<?php echo stripslashes(get_option('AL_text_contact'));?>
-		</h3>
-		<?php echo do_shortcode("[vfb id='1']"); ?>
+<section id="content" role="main">
+	<?php if($map) { ?>
+		<div class="contact-map">
+				<img src="<?php echo $map_url; ?>">
+		</div>
+	<?php } ?>
+	<div class="wrap">
+		<div class="half">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="header">
+					</header>
+					<section class="entry-content">
+						<?php echo $contact_text; ?>
+					</section>
+				</article>
+				<?php if ( ! post_password_required() ) comments_template('', true); ?>
+			<?php endwhile; endif; ?>
+		</div>
+		<div class="half">
+			<h3>
+				<?php echo stripslashes(get_option('AL_text_contact'));?>
+			</h3>
+			<?php echo do_shortcode("[vfb id='1']"); ?>
+		</div>
 	</div>
 </section>
 <?php get_footer(); ?>
